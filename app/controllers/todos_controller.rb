08ed8f4d0d6bcf471065bc5e932d9d03b6bf4c72
baptjs <baptiste.josse@gmail.com>
2022-03-31 15:41:48 +0200

    added remote: true

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -13,19 +13,22 @@ class TodosController < ApplicationController
   def update
     @todo = Todo.find(params[:id])
     @todo.update(done: params.dig(:todo, :done))
-    render 'pages/home'
+    # render 'pages/home'
+    respond_to { |format| format.js }
   end
 
   def move_higher
     @todo = Todo.find(params[:todo_id])
     @todo.move_higher
-    render 'pages/home'
+    # render 'pages/home'
+    respond_to { |format| format.js }
   end
 
   def move_lower
     @todo = Todo.find(params[:todo_id])
     @todo.move_lower
-    render 'pages/home'
+    # render 'pages/home'
+    respond_to { |format| format.js }
   end
 
   private
