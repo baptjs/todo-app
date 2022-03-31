3086e0acd9418f8755f7a5343ab46b4ea592797f
baptjs <baptiste.josse@gmail.com>
2022-03-31 17:17:23 +0200

    refactoring & tweaking front end

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,12 @@
 class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:update]
+  before_action :set_todo, only: [:move_lower, :move_higher]
+
   def create
     @todo = Todo.new(todo_params)
     @todo.user = current_user
     if @todo.save
-      respond_to { |format| format.js }
+      ajax_response
     else 
       render 'pages/home'
     end
@@ -13,26 +15,30 @@ class TodosController < ApplicationController
   def update
     @todo = Todo.find(params[:id])
     @todo.update(done: params.dig(:todo, :done))
-    respond_to { |format| format.js }
+    ajax_response
   end
 
   def move_higher
-    @todo = Todo.find(params[:todo_id])
     @todo.move_higher
-    # render 'pages/home'
-    respond_to { |format| format.js }
+    ajax_response
   end
 
   def move_lower
-    @todo = Todo.find(params[:todo_id])
     @todo.move_lower
-    # render 'pages/home'
-    respond_to { |format| format.js }
+    ajax_response
   end
 
   private
 
   def todo_params
     params.require(:todo).permit(:title, :description, :due_date)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:todo_id])
+  end
+
+  def ajax_response
+    respond_to { |format| format.js }
   end
 end
