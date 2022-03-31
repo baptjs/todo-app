5a38d8555967383165a95aba213a9430880c95fd
baptjs <baptiste.josse@gmail.com>
2022-03-31 11:11:56 +0200

    created view for Users#register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:fullname, :username, :email)
+    params.require(:user).permit(:email)
   end
 end
