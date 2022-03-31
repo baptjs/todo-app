819fc70c6455fc5b5e24a1760b2776b561a8548f
baptjs <baptiste.josse@gmail.com>
2022-03-31 11:07:42 +0200

    implemented Users#register & Users#create methods + added associated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :users, only: [:create]
+  get '/register', to: 'users#register'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
