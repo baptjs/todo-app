c67bd383db69223df491edc02092c1a93ba62bf8
baptjs <baptiste.josse@gmail.com>
2022-03-31 15:35:30 +0200

    added tasks details and stimulus function to show/hide them

--- app/javascript/controllers/update_todo_controller.js
+++ app/javascript/controllers/update_todo_controller.js
@@ -1,8 +1,13 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  // static targets = ["details"]
 
   submit() {
     event.target.closest("form").submit();
+  }
+
+  showdetails() {
+    event.target.nextElementSibling.classList.toggle('d-none');
   }
 }
