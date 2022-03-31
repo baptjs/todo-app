a1bd0c2dc4e90f15581337e2f2676cfe6b585369
baptjs <baptiste.josse@gmail.com>
2022-03-31 11:25:14 +0200

    added methods to send link to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,43 @@ class User < ApplicationRecord
   def self.find_user_by(value)
     where(email: value).first
   end
+
+  def send_login_link
+    generate_login_token
+
+    template = 'login_link'
+    UserMailer.send(template).deliver_now
+  end
+
+  def generate_login_token
+    self.token = generate_token
+    self.token_generated_at = Time.now.utc
+    save!
+  end
+
+  def login_link
+    "http://localhost:3000/auth?token=#{self.token}"
+    # "https://todo-app-31032022.herokuapp.com/auth?token=#{self.token}"
+    
+  end
+
+  def login_token_expired?
+    Time.now.utc > (self.token_generated_at + token_validity)
+  end
+
+  def expire_token!
+    self.token = nil
+    save!
+  end
+
+
+  private
+
+  def generate_token
+    SecureRandom.hex(10)
+  end
+
+  def token_validity
+    2.hours
+  end
 end
