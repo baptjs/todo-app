439947a04f1b62f0567067a71b83ba3fe48b4ecc
baptjs <baptiste.josse@gmail.com>
2022-03-31 11:02:00 +0200

    added email formatting and associated tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
+
+  before_save :format_email
+
+  def format_email
+    self.email = self.email.delete(' ').downcase
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,20 @@ RSpec.describe User, type: :model do
     expect(subject).to be_valid
   end
   it "is not valid without an email address" do
-    subject.email=nil
+    subject.email = nil
     expect(subject).to_not be_valid
   end
+
+  subject { User.create(email: "baptiste .josse@gmail. com", token: "azerty123456", token_generated_at: Date.today) }
+  
+  it "removes empty spaces from email address" do
+    expect(subject.email).to eq("baptiste.josse@gmail.com")
+  end
+
+  subject { User.create(email: "Baptiste.josse@gmail.Com", token: "azerty123456", token_generated_at: Date.today) }
+
+  it "downcases all upcased characters from email address" do
+    expect(subject.email).to eq("baptiste.josse@gmail.com")
+  end
+
 end
